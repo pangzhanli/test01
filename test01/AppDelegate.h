74a7706887a99cd62f25204e1a867be65c6b493f
pzl <pangzhanli2009@163.com>
2017-04-29 11:01:33 +0800

    第一次提交

---
+++ test01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test01
+//
+//  Created by tusm on 2017/4/29.
+//  Copyright © 2017年 oa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
